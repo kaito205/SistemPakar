186e41a032e23deb3ab78615002ba904a3b9dd6e
kaito205 <bochilandi85@gmail.com>
2026-07-13 00:42:05 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,14 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/diagnosa', function () {
+    return Inertia::render('DiagnosisForm');
+})->name('diagnosa.form');
+
+Route::get('/diagnosa/hasil', function () {
+    return Inertia::render('DiagnosisResult');
+})->name('diagnosa.result');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
